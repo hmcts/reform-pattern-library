2f90c510b8833ddad4993ce0e99f42f046c9ea83
Trevor Saint <hello@trevorsaint.com>
2017-11-08 14:30:39 +0000

    Restructuring of content. Split into individual pages ready for documentation

--- app/routes/components/index.js
+++ app/routes/components/index.js
@@ -13,4 +13,59 @@ routes.get("/" + base, function (req, res) {
 })
 
 
+routes.get("/" + base + "/address-lookup", function (req, res) {
+  res.render(base + "/address-lookup/index",
+    data = {
+      section : "components",
+      sectionName : "Components",
+      pageTitle : "Address lookup"
+    }
+  )
+})
+
+
+routes.get("/" + base + "/collapsibles", function (req, res) {
+  res.render(base + "/collapsibles/index",
+    data = {
+      section : "components",
+      sectionName : "Components",
+      pageTitle : "Collapsibles"
+    }
+  )
+})
+
+
+routes.get("/" + base + "/progress-bar", function (req, res) {
+  res.render(base + "/progress-bar/index",
+    data = {
+      section : "components",
+      sectionName : "Components",
+      pageTitle : "Progress bar"
+    }
+  )
+})
+
+
+routes.get("/" + base + "/tabs", function (req, res) {
+  res.render(base + "/tabs/index",
+    data = {
+      section : "components",
+      sectionName : "Components",
+      pageTitle : "Tabs"
+    }
+  )
+})
+
+
+routes.get("/" + base + "/add-form-items", function (req, res) {
+  res.render(base + "/add-form-items/index",
+    data = {
+      section : "components",
+      sectionName : "Components",
+      pageTitle : "Add form items"
+    }
+  )
+})
+
+
 module.exports = routes;
--- app/routes/elements/index.js
+++ app/routes/elements/index.js
@@ -13,4 +13,26 @@ routes.get("/" + base, function (req, res) {
 })
 
 
+routes.get("/" + base + "/buttons", function (req, res) {
+  res.render(base + "/buttons/index",
+    data = {
+      section : "elements",
+      sectionName : "Elements",
+      pageTitle : "Buttons"
+    }
+  )
+})
+
+
+routes.get("/" + base + "/alpha-beta-banners", function (req, res) {
+  res.render(base + "/alpha-beta-banners/index",
+    data = {
+      section : "elements",
+      sectionName : "Elements",
+      pageTitle : "Alpha and beta banners"
+    }
+  )
+})
+
+
 module.exports = routes;
