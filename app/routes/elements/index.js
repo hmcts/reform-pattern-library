81329d1817873f00f86a327788a86bf917f1771a
Trevor Saint <hello@trevorsaint.com>
2017-11-16 15:17:17 +0000

    Housekeeping

--- app/routes/elements/index.js
+++ app/routes/elements/index.js
@@ -7,18 +7,8 @@ var base = "elements";
 routes.get("/" + base, function (req, res) {
   res.render(base + "/index",
     data = {
-      pageTitle : "Elements"
-    }
-  )
-})
-
-
-routes.get("/" + base + "/buttons", function (req, res) {
-  res.render(base + "/buttons/index",
-    data = {
-      section : "elements",
-      sectionName : "Elements",
-      pageTitle : "Buttons"
+      pageTitle : "Elements",
+      pageStrapline: "A collection of elements for the Reform Programme."
     }
   )
 })
@@ -40,29 +30,19 @@ routes.get("/" + base + "/form-elements", function (req, res) {
     data = {
       section : "elements",
       sectionName : "Elements",
-      pageTitle : "Form elements"
+      pageTitle : "Form elements",
+      pageStrapline: "A collection of form patterns."
     }
   )
 })
 
 
-routes.get("/" + base + "/dates", function (req, res) {
-  res.render(base + "/dates/index",
+routes.get("/" + base + "/buttons", function (req, res) {
+  res.render(base + "/buttons/index",
     data = {
       section : "elements",
       sectionName : "Elements",
-      pageTitle : "Dates"
-    }
-  )
-})
-
-
-routes.get("/" + base + "/names", function (req, res) {
-  res.render(base + "/names/index",
-    data = {
-      section : "elements",
-      sectionName : "Elements",
-      pageTitle : "Names"
+      pageTitle : "Buttons"
     }
   )
 })
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,43 +3,12 @@ const routes = require("express").Router();
 
 routes.use("/", require("./elements"))
 routes.use("/", require("./components"))
-routes.use("/", require("./layouts"))
-routes.use("/", require("./journeys"))
 
 
 routes.get("/", function (req, res) {
   res.render("index",
     data = {
       pageTitle : "Reform Pattern Library"
-    }
-  )
-})
-
-
-routes.get("/macros", function (req, res) {
-  res.render("macros",
-    data = {
-      pageTitle : "Macros collection"
-    }
-  )
-})
-
-
-// add form items
-routes.get("/add-form-items", function (req, res) {
-  res.render("add-form-items/index",
-    data = {
-      pageTitle : "Example: Add form items"
-    }
-  )
-})
-
-
-// 404
-routes.get("/*", function (req, res) {
-  res.render("404/index",
-    data = {
-      pageTitle : "Page not found"
     }
   )
 })
