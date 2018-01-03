86351d2fdfc1b994e1f7cf982a21a1f8d099e1f9
Trevor Saint <hello@trevorsaint.com>
2018-01-03 15:25:13 +0000

    Added links for new items added into pages

--- app/routes/pages/index.js
+++ app/routes/pages/index.js
@@ -7,7 +7,19 @@ var base = "pages";
 routes.get("/" + base, function (req, res) {
   res.render(base + "/index",
     data = {
-      pageTitle : "Pages"
+      pageTitle : "Pages",
+      pageStrapline: "A collection of pages for the Reform Programme."
+    }
+  )
+})
+
+
+routes.get("/" + base + "/task-list", function (req, res) {
+  res.render(base + "/task-list/index",
+    data = {
+      section : "pages",
+      sectionName : "Pages",
+      pageTitle : "Task list"
     }
   )
 })
@@ -16,6 +28,8 @@ routes.get("/" + base, function (req, res) {
 routes.get("/" + base + "/check-your-answers", function (req, res) {
   res.render(base + "/check-your-answers/index",
     data = {
+      section : "pages",
+      sectionName : "Pages",
       pageTitle : "Check your answers"
     }
   )
